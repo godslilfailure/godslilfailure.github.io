c4389a5229490f77b177d08c90b3c09f2244504e
godslilfailure <118316111+godslilfailure@users.noreply.github.com>
2022-11-15 20:40:25 -0300

    alterado pra enviar o email

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
 
-let teste;
 
 
-
-$( document ).ready(function() {
-    console.log('uhul');
-    teste = $('#teste')
-    mudaONomeDaPica();
-});
-
-
-function mudaONomeDaPica(){
-
-$.ajax({
-    url: "https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json",
-    success: function( result ) {
-        teste[0].innerText=result.cand[0].pvap
-    }
-  });
-}
